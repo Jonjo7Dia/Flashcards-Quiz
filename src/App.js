97a87b3d88b33098494771bebf74fd4c0b09ad1c
Jonathan <jonosphgames@gmail.com>
2022-02-23 21:51:24 -0300

    Moving on to Home Page

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import HomePage from "./pages/HomePage";
 import CreateNewDeck from "./pages/CreateNewDeck";
 import Favorites from "./pages/Favorites";
 import Layout from "./components/layout/Layout";
+import { useDispatch } from "react-redux";
 
 function App() {
+ const dispatch =  useDispatch()
+ dispatch()
   return (
     <Layout>
       <Routes>
